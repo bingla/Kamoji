3c3ec864857860ded00aa417e06965d733a9e1da
Pierre Nilsson <bingla@users.noreply.github.com>
2021-02-12 17:45:17 +0100

    Develop (#4)
    * Split test
    
    * Added test for helper
    
    * Bingla patch 1 (#2)
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Bingla patch 1 (#3)
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml

---
+++ Kamoji.Test/KamojiTest_Negative.cs
@@ -0,0 +1,37 @@
+﻿using Kamoji.Interfaces;
+using Kamoji.Internals;
+using Kamoji.Services;
+using Moq;
+using Xunit;
+
+namespace Kamoji.Test
+{
+    public class KamojiTest_Negative
+    {
+        private Mock<IRandomizer> _randomizer;
+
+        public KamojiTest_Negative()
+        {
+            _randomizer = new Mock<IRandomizer>();
+            _randomizer.Setup(r => r.Next(0)).Returns(0);
+        }
+
+        [Fact]
+        public void ShouldReturnNegativeCategory()
+        {
+            IKamojiService kamoji = new KamojiService(_randomizer.Object);
+
+            var anger = kamoji.Negative(eNegative.Anger);
+            var dissatisfaction = kamoji.Negative(eNegative.Dissatisfaction);
+            var fear = kamoji.Negative(eNegative.Fear);
+            var pain = kamoji.Negative(eNegative.Pain);
+            var sadness = kamoji.Negative(eNegative.Sadness);
+
+            Assert.Equal(Emoji.Negative[eNegative.Anger][0], anger);
+            Assert.Equal(Emoji.Negative[eNegative.Dissatisfaction][0], dissatisfaction);
+            Assert.Equal(Emoji.Negative[eNegative.Fear][0], fear);
+            Assert.Equal(Emoji.Negative[eNegative.Pain][0], pain);
+            Assert.Equal(Emoji.Negative[eNegative.Sadness][0], sadness);
+        }
+    }
+}
--- Kamoji.Test/KamojiTest_Positive.cs
+++ Kamoji.Test/KamojiTest_Positive.cs
@@ -6,11 +6,11 @@ using Xunit;
 
 namespace Kamoji.Test
 {
-    public class KamojiTest
+    public class KamojiTest_Positive
     {
         private Mock<IRandomizer> _randomizer;
 
-        public KamojiTest()
+        public KamojiTest_Positive()
         {
             _randomizer = new Mock<IRandomizer>();
             _randomizer.Setup(r => r.Next(0)).Returns(0);
@@ -31,24 +31,6 @@ namespace Kamoji.Test
             Assert.Equal(Emoji.Positive[ePositive.Joy][0], joy);
             Assert.Equal(Emoji.Positive[ePositive.Love][0], love);
             Assert.Equal(Emoji.Positive[ePositive.Sympathy][0], sympathy);
-        }
-
-        [Fact]
-        public void ShouldReturnNegativeCategory()
-        {
-            IKamojiService kamoji = new KamojiService(_randomizer.Object);
-
-            var anger = kamoji.Negative(eNegative.Anger);
-            var dissatisfaction = kamoji.Negative(eNegative.Dissatisfaction);
-            var fear = kamoji.Negative(eNegative.Fear);
-            var pain = kamoji.Negative(eNegative.Pain);
-            var sadness = kamoji.Negative(eNegative.Sadness);
-
-            Assert.Equal(Emoji.Negative[eNegative.Anger][0], anger);
-            Assert.Equal(Emoji.Negative[eNegative.Dissatisfaction][0], dissatisfaction);
-            Assert.Equal(Emoji.Negative[eNegative.Fear][0], fear);
-            Assert.Equal(Emoji.Negative[eNegative.Pain][0], pain);
-            Assert.Equal(Emoji.Negative[eNegative.Sadness][0], sadness);
         }
     }
 }
